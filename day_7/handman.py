257ac933db38c7a75ad2cadb27abe17caec3e656
Luis Nieto hueso <72223842+luisnietohueso@users.noreply.github.com>
2023-05-13 22:19:55 +0100

    Add files via upload

---
+++ day_7/handman.py
@@ -0,0 +1,57 @@
+import random
+import handmanlist
+
+
+def handan():
+    end_of_game = False
+    word_list = handmanlist.word_lis
+    chosen_word = random.choice(word_list)
+    word_length = len(chosen_word)
+
+    # TODO-1: - Create a variable called 'lives' to keep track of the number of lives left.
+    # Set 'lives' to equal 6.
+    lives = 6
+    print(handmanlist.logo)
+    # Testing code
+    print(f'Pssst, the solution is {chosen_word}.')
+
+    # Create blanks
+    display = []
+    for _ in range(word_length):
+        display += "_"
+
+    while not end_of_game:
+        guess = input("Guess a letter: ").lower()
+        if guess in display:
+            print("You've already guessed that letter.")
+            continue
+
+        # Check guessed letter
+        for position in range(word_length):
+            letter = chosen_word[position]
+            # print(f"Current position: {position}\n Current letter: {letter}\n Guessed letter: {guess}")
+            if letter == guess:
+                display[position] = letter
+
+        if guess not in chosen_word:
+            print(f'You guessed {guess} that is not the word you lost a live')
+            lives -= 1
+            if lives == 0:
+                end_of_game = True
+                print('you lose.')
+            print('your total lives are:', lives)
+
+        # TODO-2: - If guess is not a letter in the chosen_word,
+        # Then reduce 'lives' by 1.
+        # If lives goes down to 0 then the game should stop and it should print "You lose."
+
+        # Join all the elements in the list and turn it into a String.
+        print(f"{' '.join(display)}")
+
+        # Check if user has got all letters.
+        if "_" not in display:
+            end_of_game = True
+            print("You win.")
+        # TODO-3: - print the ASCII art from 'stages' that corresponds to the current number of 'lives' the user has
+        #  remaining.
+        print(handmanlist.stages[lives])
